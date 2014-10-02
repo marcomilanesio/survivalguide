50004c3957d7e627518b8f0bd3f0bb493b089541
Marco Milanesio <milanesio.marco@gmail.com>
2014-10-02 16:25:47 +0200

    Signed-off-by: Marco Milanesio <milanesio.marco@gmail.com>

---
+++ survivalguide/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+from .views import HomePageView, SignUpView, LoginView, LogOutView
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'survivalguide.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', HomePageView.as_view(), name='home'),  # REGEXP for matching
+    url(r'^accounts/register/$', SignUpView.as_view(), name='signup'),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', LogOutView.as_view(), name='logout'),
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ survivalguide/views.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.views import generic
+from django.core.urlresolvers import reverse_lazy
+
+from braces import views
+
+from .forms import RegistrationForm, LoginForm
+
+class HomePageView(generic.TemplateView):
+    template_name = 'home.html'
+
+
+class SignUpView(views.AnonymousRequiredMixin, views.FormValidMessageMixin, generic.CreateView):
+    form_class = RegistrationForm
+    form_valid_message = 'Thanks for signing up, go ahead and login.'
+    model = User
+    template_name = 'accounts/signup.html'
+
+
+class LoginView(views.AnonymousRequiredMixin, views.FormValidMessageMixin, generic.FormView):
+    form_class = LoginForm
+    form_valid_message = 'You are logged in now.'
+    success_url = reverse_lazy('home')
+    template_name = 'accounts/login.html'
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(self.request, user)
+            return super(LoginView, self).form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+
+class LogOutView(views.LoginRequiredMixin, views.MessageMixin, generic.RedirectView):
+    url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        self.messages.success("You've been logged out. Come back soon!")
+        return super(LogOutView, self).get(request, *args, **kwargs)
